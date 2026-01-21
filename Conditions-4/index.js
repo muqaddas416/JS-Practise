0a00d721f60fbcbc3124a22f5d1f389746fb0f32
muqaddas416 <muqaddasj416@gmail.com>
2026-01-21 15:04:38 -0800

    Conditions
    1 if
    2 if-else
    3 else-if
    4 switch

---
+++ Conditions-4/index.js
@@ -0,0 +1,50 @@
+// conditions
+            // if
+// let x= 100;
+// if(x>10){
+//     console.log("x is greater than 10");
+// }else{
+//     console.log("x is not greater than 10")
+// }
+
+// Exampel with and OR operator
+                // if - else
+//  let age= 18;
+//  let dl= true;
+//  if(age >18 || dl == true){
+//       console.log("you can drive");
+//  }else{
+//     console.log("you cannot drive");
+//  }
+
+                 //  else-if
+//     let experience = 4
+// if(experience<=1){
+//     console.log("Fresher Dev");
+// }else if(experience<=3){
+//     console.log("senior Dev");
+// }else{
+//     console.log("In her strugglin era like me")
+// }
+
+           // Switch
+
+           let day = 0
+
+           switch(day){
+            case 1: console.log("Monday");
+            break;
+            case 2: console.log("Tuesday");
+            break;
+            case 3: console.log("Wed");
+            break;
+            case 4: console.log("Thur");
+            break;
+            case 5: console.log("Fri");
+            break;
+            case 6: console.log("Sat");
+            break;
+            case 7: console.log("Sun");
+            break;
+            default: console.log ("invalid number");
+            }
