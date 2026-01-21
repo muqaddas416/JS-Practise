491f9963db19f7b04ffda703ac71431de51e2133
muqaddas416 <muqaddasj416@gmail.com>
2026-01-21 11:59:17 -0800

    Variabels
    variabels and data types

---
+++ Variabels-DataType-2/index.js
@@ -0,0 +1,33 @@
+//variabels
+// var
+// let
+// const
+
+
+// const = fixed value (change nahi ho sakti)
+
+// let = change ho sakti hai
+
+// var = old way (avoid)
+// thats was the reason we cannot use var because we cannot 
+// assign same roll number two different peoples
+// var x = 10;
+// var x =20;
+// console.log(x)
+// console.log(typeof(x));
+   
+
+// let cannot be redeclared but we can updated it
+
+// let x = 20;
+// x=30;
+// console.log(x);
+
+
+// const can not b redeclared or updated
+
+// const x = 3.14
+// x=4.5
+// console.log(x)
+let z;
+console.log(z);
--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("Muqaddas write the code")
