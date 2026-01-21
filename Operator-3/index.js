877cd466a48f167c231c088a9a7d63a91db94061
muqaddas416 <muqaddasj416@gmail.com>
2026-01-21 13:31:42 -0800

    Operators
    5 type of operator

---
+++ Operator-3/index.js
@@ -0,0 +1,57 @@
+//Operators are used to perform some operations on data 
+//5 Types of Operatoe
+                     // 1- Arithematic
+// => +, -, %, /, *
+// . Increment
+// . Decrement
+// . Modulus
+// . Divide
+// . Multiply
+//  
+                         // 2- Assigment Operator
+//    . =
+//    . +=
+//    . -=
+//    . *=
+//    . %=
+//    . **=          
+
+
+// let x = 10;
+// let sum = 20;
+// sum = sum + x;
+// sum = sum * x;
+// sum += x;
+// sum -= x;
+// sum *= x;
+// sum %= x;
+// sum **= x;
+// console.log(sum);
+
+                      // 3- Comparison Operator
+    //    ==, !==, !=, ===, >, <, >=, <=,
+
+    //  . == (Compare values only)
+    //  . != (Not Equal values)
+    //  . !== (Not Equal values type)
+    // . ===(compare values and data types)
+    
+    // let x= 10;
+    // let y= "10";
+    // console.log(x === y)
+                
+                       // 4- Logical Operator
+                    //    these operator are used in if-else
+    //  . Logical AND &&
+    //  . Logical OR ||
+    //  . Logical Not !
+                          // 5-Ternary Operator
+    
+    //     ()      ?   ()  :  ()
+    // Condition  ?  True : False
+
+//i.e  
+// let marks = 40;
+// const result = (marks >= 40) ? ("Pass" ): ("Fail ")
+// console.log(result);
+    
