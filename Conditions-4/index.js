c6d863e588c6caa04890b8e757feaee50d5b1161
muqaddas416 <muqaddasj416@gmail.com>
2026-01-22 21:22:43 -0800

    Loops
    Basic Loops
    1-for loop
    2-while-loop
    3-do-while loop

--- Conditions-4/index.js
+++ Conditions-4/index.js
@@ -29,22 +29,22 @@
 
            // Switch
 
-           let day = 0
+        //    let day = 0
 
-           switch(day){
-            case 1: console.log("Monday");
-            break;
-            case 2: console.log("Tuesday");
-            break;
-            case 3: console.log("Wed");
-            break;
-            case 4: console.log("Thur");
-            break;
-            case 5: console.log("Fri");
-            break;
-            case 6: console.log("Sat");
-            break;
-            case 7: console.log("Sun");
-            break;
-            default: console.log ("invalid number");
-            }
+        //    switch(day){
+        //     case 1: console.log("Monday");
+        //     break;
+        //     case 2: console.log("Tuesday");
+        //     break;
+        //     case 3: console.log("Wed");
+        //     break;
+        //     case 4: console.log("Thur");
+        //     break;
+        //     case 5: console.log("Fri");
+        //     break;
+        //     case 6: console.log("Sat");
+        //     break;
+        //     case 7: console.log("Sun");
+        //     break;
+        //     default: console.log ("invalid number");
+        //     }
---
+++ Loops-5/index.js
@@ -0,0 +1,50 @@
+// Loops are used to execute a piece of code again and again
+// six types of loop
+// 1-for
+// 2-while
+// 3-do-while
+// 4-for-in
+// 5-for-of
+// 6-for each
+
+            // 1 For loop
+    // for loop is commonly used when the number of iterations is known
+    // it consists of three parts intialization,condition,and final experrission
+      
+
+    // for(intialization,condition,incremet)
+        // block of code that need to b executed
+    // i++ = i+1
+    // i-- = i-1
+
+    // for(let i=0;i<5;i++){
+    //     console.log("Muqaddas", i);
+    // }
+
+    //how to print a table
+    // let n=2;
+    // for(let i=1;i<=10;i++){
+    //     console.log(`${n} X ${i} =`, n*i)
+    // }
+    
+    //    while loop
+    // Repeat a condition of block until the condition is true 
+    // number of iteration is not specified here
+
+    // while(condition){
+        // condition run until condition becomes true
+    // }
+
+    //   let i = 1;
+    //   while(i<=5){
+    //     console.log(i);
+    //     i++
+
+    //   }
+//A do…while loop runs at least one time, even if the condition is false.
+ 
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
+// }while(i<=5);
